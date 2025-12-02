10a46f28ab409cc3078f551a8c83a2622bb121d7
Gabriel <chamorrog12@gmail.com>
2025-12-02 15:36:56 -0500

    Keys

--- ExamenBi/Program.cs
+++ ExamenBi/Program.cs
@@ -1,7 +1,5 @@
+using ExamenBi.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-using ExamenBi.Data;
 
 namespace ExamenBi
 {
@@ -9,33 +7,25 @@ namespace ExamenBi
     {
         public static void Main(string[] args)
         {
-            var miConexion = new ConexionDB();
             var builder = WebApplication.CreateBuilder(args);
 
-            var connectionString = builder.Configuration.GetConnectionString("LigaApiContext.sqlserver");
-
-            builder.Services.AddDbContext<LigaApiContext>(options =>
-                options.UseSqlServer(connectionString));
-            // Add services to the container.
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")
+                ));
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+          
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
 
             app.MapControllers();
 
